3c69a2324ec20450b070f1bcbf6ec2b5f165266d
azimkhansherkhan <62377496+azimkhansherkhan@users.noreply.github.com>
2020-05-07 13:26:17 +0600

    new

---
+++ rpr3.php
@@ -0,0 +1,26 @@
+<?php
+$db_connection = pg_connect("host=localhost dbname=sellcardb user=sellcaru password=q1");
+$result = pg_query($db_connection, "SELECT * FROM electrocar");
+
+$resultArr = pg_fetch_all($result);
+//print_r($resultArr);
+
+echo '<table>
+        <tr>
+         <td>Marka</td>
+         <td>Model</td>
+		 <td>God vypuska</td>
+		 <td>Prodano</td>
+        </tr>';
+
+foreach($resultArr as $array)
+{
+    echo '<tr>
+            <td>'. $array['marka'].'</td>
+            <td>'. $array['model'].'</td>
+			<td>'. $array['god_vypuska'].'</td>
+			<td>'. $array['prodano'].'</td>
+          </tr>';
+}
+echo '</table>';
+?>	
